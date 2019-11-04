80947a7f545f12dc45389578eef27e6cd1b84678
rebla <rebla@192.168.1.49>
2019-11-04 13:52:01 +0100

    Task-014 Completed

---
+++ src/main/java/acme/forms/Chart.java
@@ -0,0 +1,23 @@
+
+package acme.forms;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Chart implements Serializable {
+
+	/**
+	 *
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	Object[]					totalNumberOfCompaniesBySector;
+	Object[]					totalNumberOfInvestorsBySector;
+	Object[]					investorSector;
+	Object[]					companySector;
+
+}
